16e33ca2e22d5ab28943793f927a21cb56ce6ffc
mr.huo <455801665@qq.com>
2020-09-03 20:54:46 +0800

    â加入â

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12528,7 +12528,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55146" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56401" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
