bbe640d4f9de25d320477e740e002b620549a557
mr.huo <455801665@qq.com>
2020-09-06 10:29:48 +0800

    验证iconPosition为left或right中的一个

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,18 @@
 </template>
 <script>
 export default {
-  props: ['icon', 'iconPosition']
+  // props: ['icon', 'iconPosition']
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value){
+        console.log(value);
+        return !(value !== 'left' && value !== 'right');
+      }
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -25,14 +36,38 @@ export default {
   justify-content: center;
   align-items: center;
   vertical-align: middle;
-  &:hover {border-color: var(--border-color-hover);}
-  &:active {background-color: var(--button-active-bg);}
-  &:focus {outline: none;}
-  > .content {order: 2;}
-  > .icon {order: 1; margin-right: .3em;}
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+
   &.icon-right {
-    > .content {order: 1;}
-    > .icon {order: 2; margin-left: .3em; margin-right: 0}
+    > .content {
+      order: 1;
+    }
+
+    > .icon {
+      order: 2;
+      margin-left: .3em;
+      margin-right: 0
+    }
   }
 }
 </style>
