a750d12add698047a8549e3c35189605d88b3009
mr.huo <455801665@qq.com>
2020-09-13 12:45:34 +0800

    row和col的雏形

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div class="row">
+        <slot/>
+    </div>
 </template>
 
 <script>
@@ -8,6 +10,9 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .row{
+        display: flex;
+    }
 
 </style>
