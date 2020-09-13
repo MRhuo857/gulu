ca553f833b5153c7808b482370501b73eb5840a2
mr.huo <455801665@qq.com>
2020-09-13 15:49:45 +0800

    重构row和col

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,7 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-         :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-        <div style="border: 1px solid green; height: 100px;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass"
+         :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 <script>
@@ -17,11 +15,23 @@
         type: [Number, String]
       },
     },
-    data () {
+    data() {
       return {
         gutter: 0
       }
     },
+    computed: {
+      colClass() {
+        let {span, offset} = this
+        return [span && `col-${span}`, offset && `offset-${offset}`]
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px'
+        }
+      }
+    }
 
   }
 </script>
