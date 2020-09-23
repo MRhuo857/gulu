13bbe0e18ff218cbd862d75d2b4ebdb3094ac2a1
mr.huo <455801665@qq.com>
2020-09-23 20:49:20 +0800

    将弹出的内容插到body里

--- src/popover.vue
+++ src/popover.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div v-if="visible" class="content-wrapper" @click.stop>
+        <div ref="contentWrapper" v-if="visible" class="content-wrapper">
             <slot name="content"></slot>
         </div>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
 
-        <slot></slot>
     </div>
 </template>
 
@@ -14,23 +16,32 @@
     data() {
       return {visible: false}
     },
+    mounted() {
+
+
+    },
     methods: {
       xxx() {
         this.visible = !this.visible
-        if (this.visible===true){
-          this.$nextTick(()=>{
-            let eventHandler=()=>{
-              this.visible=false
+        if (this.visible === true) {
+          this.$nextTick(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            console.log(width, height, top, left);
+            this.$refs.contentWrapper.style.left = left+'px'
+            this.$refs.contentWrapper.style.top = top+'px'
+            let eventHandler = () => {
+              this.visible = false
               console.log('document 隐藏popover')
-              document.removeEventListener('click',eventHandler)
+              document.removeEventListener('click', eventHandler)
             }
-            document.addEventListener('click',eventHandler)
+            document.addEventListener('click', eventHandler)
           })
-        }else{
+        } else {
           console.log('vm隐藏popover')
         }
       }
-    }
+    },
   }
 </script>
 
@@ -39,13 +50,12 @@
         display: inline-block;
         vertical-align: top;
         position: relative;
-        >.content-wrapper{
-            border: 1px solid red;
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            box-shadow: 0 0 2px rgba(0,0,0,0.5);
-        }
+    }
+     .content-wrapper {
+         position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 2px rgba(0, 0, 0, 0.5);
+         transform: translateY(-100%);
     }
 
 </style>
