16dd203ef759bc86fa93ff8f3410f1450e7c3eff
mr.huo <455801665@qq.com>
2020-09-24 19:54:13 +0800

    功能方面基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,61 +1,68 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div ref="contentWrapper" v-if="visible" class="content-wrapper">
+    <div class="popover" @click="onClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
-            <slot></slot>
-        </span>
-
+      <slot></slot>
+    </span>
     </div>
 </template>
 
 <script>
   export default {
     name: "GuluPopover",
-    data() {
+    data () {
       return {visible: false}
     },
-    mounted() {
-
-
-    },
     methods: {
-      xxx() {
-        this.visible = !this.visible
-        if (this.visible === true) {
-          this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper)
-            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            console.log(width, height, top, left);
-            this.$refs.contentWrapper.style.left = left+'px'
-            this.$refs.contentWrapper.style.top = top+'px'
-            let eventHandler = () => {
-              this.visible = false
-              console.log('document 隐藏popover')
-              document.removeEventListener('click', eventHandler)
-            }
-            document.addEventListener('click', eventHandler)
-          })
-        } else {
-          console.log('vm隐藏popover')
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        this.close()
+      },
+      open () {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close () {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
+          }
         }
       }
-    },
+    }
   }
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
     .popover {
         display: inline-block;
         vertical-align: top;
         position: relative;
     }
-     .content-wrapper {
-         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 2px rgba(0, 0, 0, 0.5);
-         transform: translateY(-100%);
-    }
 
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
+    }
 </style>
