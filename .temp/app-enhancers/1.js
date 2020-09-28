7fc8b33791967923eace63b42276ac2fb36ff25c
mr.huo <455801665@qq.com>
2020-09-28 21:57:16 +0800

    创建官网雏形

---
+++ .temp/app-enhancers/1.js
@@ -0,0 +1,2 @@
+import "C:\\Users\\mr.huo\\Desktop\\gulu\\node_modules\\@vuepress\\plugin-nprogress\\enhanceAppFile.js"
+export default {}
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: '极简UI',
+  description: '一个好用的UI框架',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '入门',
+        collapsable: false,
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        collapsable: false,
+        children: [
+          '/components/button'
+        ]
+      },
+
+    ]
+  }
+}
