79ac8f67034856dc10185d3ed7a992f791de131e
mr.huo <455801665@qq.com>
2020-10-02 11:40:15 +0800

    解决tabs的bug1

--- docs/.vuepress/components/tab-demos.vue
+++ docs/.vuepress/components/tab-demos.vue
@@ -1,45 +1,50 @@
 <template>
-    <div style="padding-top: 16px;">
-        <g-tabs :selected="selected">
+    <div>
+        <g-tabs :selected.sync="selectedTab">
             <g-tabs-head>
-                <g-tabs-item name="1">1</g-tabs-item>
-                <g-tabs-item name="2">2</g-tabs-item>
+                <g-tabs-item name="tab1">
+                    <g-icon name="settings"></g-icon>
+                    Tab1
+                </g-tabs-item>
+                <g-tabs-item name="tab2">
+                    Tab2
+                </g-tabs-item>
+                <g-tabs-item name="tab3" disabled>
+                    Tab3
+                </g-tabs-item>
             </g-tabs-head>
             <g-tabs-body>
-                <g-tabs-pane name="1">content 1</g-tabs-pane>
-                <g-tabs-pane name="2">content 2</g-tabs-pane>
+                <g-tabs-pane name="tab1">
+                    内容1
+                </g-tabs-pane>
+                <g-tabs-pane name="tab2">
+                    内容2
+                </g-tabs-pane>
+                <g-tabs-pane name="tab3">
+                    内容3
+                </g-tabs-pane>
             </g-tabs-body>
         </g-tabs>
     </div>
 </template>
 <script>
-  import GTabs from '../../../src/tabs'
-  import GTabsBody from '../../../src/tabs-body'
-  import GTabsHead from '../../../src/tabs-head'
-  import GTabsItem from '../../../src/tabs-item'
-  import GTabsPane from '../../../src/tabs-pane'
-
+  import Tabs from "../../../src/tabs";
+  import TabsHead from "../../../src/tabs-head";
+  import TabsBody from "../../../src/tabs-body";
+  import TabsItem from "../../../src/tabs-item";
+  import TabsPane from "../../../src/tabs-pane";
+  import Icon from "../../../src/icon";
   export default {
-    components: {GTabs, GTabsBody, GTabsHead, GTabsItem, GTabsPane},
+    components: {
+      "g-tabs": Tabs,
+      "g-tabs-head": TabsHead,
+      "g-tabs-body": TabsBody,
+      "g-tabs-item": TabsItem,
+      "g-tabs-pane": TabsPane,
+      "g-icon": Icon,
+    },
     data() {
-      return {
-        selected: '1',
-        content: `
-          data:{
-            selected: '1'
-          }
-          <g-tabs :selected="selected">
-            <g-tabs-head>
-              <g-tabs-item name="1">1</g-tabs-item>
-              <g-tabs-item name="2">2</g-tabs-item>
-            </g-tabs-head>
-            <g-tabs-body>
-              <g-tabs-pane name="1">content 1</g-tabs-pane>
-              <g-tabs-pane name="2">content 2</g-tabs-pane>
-            </g-tabs-body>
-          </g-tabs>
-      `.replace(/^ {8}/gm, '').trim()
-      }
-    }
-  }
+      return { selectedTab: "tab2" };
+    },
+  };
 </script>
--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -10,7 +10,7 @@
 </template>
 <script>
   export default {
-    name: "MgTabsHead",
+    name: "GuluTabsHead",
     inject: ["eventBus"],
     mounted() {
       this.eventBus.$on("update:selected", (item, vm) => {
